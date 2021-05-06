cba18f07af85247283f289987dde827f7afd3e2c
dxm <dxm@123.cn>
2021-05-06 20:46:01 +0800

    11-实现树形的静态组织架构

---
+++ src/views/departments/components/tree-tool.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- 用了一个行列布局 -->
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px; width: 100%">
+    <el-col>
+
+      <strong v-if="isroot">
+        <i class="el-icon-s-platform" style="font-size:20px" />{{ treenode.name }}
+      </strong>
+      <span>{{ treenode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 两个内容 -->
+        <el-col>{{ treenode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isroot">编辑子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isroot">删除子部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    treenode: {
+      type: Object,
+      required: true
+    },
+    isroot: {
+      type: Boolean,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/departments/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="tree-card">
+
+        <treeTools :treenode="company" :isroot="true" />
+
+        <el-tree :data="departs" :props="{lable:'name'}" :default-expand-all="true">
+          <template #default="scpoed">
+            <treeTools :treenode="scpoed.data" :isroot="false" />
+          </template>
+        </el-tree>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import treeTools from './components/tree-tool'
+export default {
+  components: {
+    treeTools
+  },
+  data() {
+    return {
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
+      },
+      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
+        { name: '行政部', manager: '刘备' },
+        { name: '人事部', manager: '孙权' }]
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tree-card {
+  padding: 30px  70px;
+  font-size:14px;
+}
+</style>
